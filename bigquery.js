b7c8f95711ce790b8bb015fe533547426f9d2dd6
1cadumagalhaes <1cadumagalhaes@users.noreply.github.com>
2021-06-29 22:19:30 +0000

    style: :lipstick: Apply formatting changes

--- bigquery.js
+++ bigquery.js
@@ -3,36 +3,33 @@ const { BigQuery } = require('@google-cloud/bigquery');
 let schemaExample = [
   {
     name: 'Column name',
-    type: 'STRING',//INTEGER,FLOAT,BOOLEAN,RECORD,
-    mode: 'REQUIRED' //opcional
-  }
-]
+    type: 'STRING', //INTEGER,FLOAT,BOOLEAN,RECORD,
+    mode: 'REQUIRED', //opcional
+  },
+];
 
 /**
  * Returns a bigquery dataset. If it doesn't exist, creates a new and then returns.
- * @param {String} datasetId 
- * @returns 
+ * @param {String} datasetId
+ * @returns
  */
 async function getOrCreateDataset(datasetId) {
   const bq = new BigQuery();
   return bq.dataset(datasetId).get({ autoCreate: true });
-
 }
 
-
 async function getOrCreateTable({ datasetId, tableName, schema }) {
-
   try {
     const dataset = await getOrCreateDataset(datasetId);
 
     const options = {
       schema,
       location: 'US',
-      type: 'TABLE'
+      type: 'TABLE',
     };
     return dataset.table(tableName).get({
       autoCreate: true,
-      ...options
+      ...options,
     });
   } catch (error) {
     console.error(`Error getting table ${tableName}:`, error);
@@ -43,12 +40,11 @@ async function getOrCreateTable({ datasetId, tableName, schema }) {
 /**
  * Insert rows as stream at the given table and dataset.
  * @param {Array} rows - Array of objects that will be inserted
- * @param {*} datasetId 
- * @param {*} tableName 
+ * @param {*} datasetId
+ * @param {*} tableName
  * @param {*} schema - Schema of the table, in case you need to create it.
  */
 async function insertRowsAsStream(rows, datasetId, tableName, schema) {
-
   try {
     const table = await getOrCreateTable({ datasetId, tableName, schema });
     await table.insert(JSON.stringify(rows));
@@ -56,10 +52,9 @@ async function insertRowsAsStream(rows, datasetId, tableName, schema) {
   } catch (error) {
     console.error(`Error inserting ${rows.lenght} in ${tableId}. \n`, error);
   }
-
 }
 
 exports = {
   getOrCreateTable,
-  insertRowsAsStream
-}
+  insertRowsAsStream,
+};
--- index.js
+++ index.js
@@ -8,37 +8,35 @@ const app = express();
 const port = 8000;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Acces-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Acces-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
-})
-
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 app.use(cors());
 
 const dimensions = {
-  "00": "Não há ocorrências",
-  "01": "Erro de disponibilidade",
-  "02": "Erro de tempestividade",
-  "03": "Erro de completude",
-  "04": "Erro de quantidade",
-  "05": "Erro de validade",
-  "06": "Erro de consistencia",
-  "07": "Erro de acurácia",
-  "08": "Erro de uniformidade",
-  "09": "Erro de acessibilidade",
-  "10": "Erro de segurança"
-}
+  '00': 'Não há ocorrências',
+  '01': 'Erro de disponibilidade',
+  '02': 'Erro de tempestividade',
+  '03': 'Erro de completude',
+  '04': 'Erro de quantidade',
+  '05': 'Erro de validade',
+  '06': 'Erro de consistencia',
+  '07': 'Erro de acurácia',
+  '08': 'Erro de uniformidade',
+  '09': 'Erro de acessibilidade',
+  10: 'Erro de segurança',
+};
 
 /**
- * 
- * @param {string} [project=GCP Project] - Name of the project. 
+ *
+ * @param {string} [project=GCP Project] - Name of the project.
  * @param {string} module - Name of the module that sent the message.
- * @param {string} spec - Division of the module 
+ * @param {string} spec - Division of the module
  * @param {number} deploy - Module or Cloud Function version
  * @param {string} [code="00-00"] - Status code, will be used to get the message.
  * @param {string} [description] - Description of the status
@@ -46,26 +44,36 @@ const dimensions = {
  * @param {Object} [payload] - JSON payload that will be passed without changing the table structure
  * @returns {Object} - Structured message.
  */
-function hubMessage(project = process.env.GCLOUD_PROJECT, module = "none", spec = "", deploy = 1, code = "00-00", description = "", details = "", payload = {}) {
+function hubMessage(
+  project = process.env.GCLOUD_PROJECT,
+  module = 'none',
+  spec = '',
+  deploy = 1,
+  code = '00-00',
+  description = '',
+  details = '',
+  payload = {}
+) {
   return {
     timestamp: new Date(),
     jobId: cuid(), //UUID ou CUID talvez? ou algo mais simples mesmo
     project: project, //cliente/projeto
     module: module, //identificador do módulo que mandou a mensagem
     spec: spec,
-    deploy: deploy, //versão atual do módulo ou da cloud function 
-    version: "0.0.1", //Versão do hub,
+    deploy: deploy, //versão atual do módulo ou da cloud function
+    version: '0.0.1', //Versão do hub,
     status: {
       code: code,
       message: dimensions[code.split('-')[0]],
       description: description,
-      details: details
+      details: details,
     },
-    payload: JSON.stringify(payload)
+    payload: JSON.stringify(payload),
   };
 }
 
-app.route('/')
+app
+  .route('/')
   .get(function (req, res) {
     const message = JSON.stringify(hubMessage(), null, 2);
     const response = `This is a example message:\n${message}`;
@@ -74,13 +82,12 @@ app.route('/')
   })
   .post(function (req, res) {
     try {
-      if (!req.body) throw "No body found on the request";
+      if (!req.body) throw 'No body found on the request';
       const body = JSON.parse(req.body);
       let keyNotInObject = (key) => !body.hasOwnProperty(key);
       let required_keys = ['module', 'spec', 'deploy'];
-      required_keys = required_keys.filter(keyNotInObject)
-      if (required_keys.length > 0)
-        throw `Missing required keys: ${required_keys.join(', ')}`;
+      required_keys = required_keys.filter(keyNotInObject);
+      if (required_keys.length > 0) throw `Missing required keys: ${required_keys.join(', ')}`;
 
       let { project, module, spec, deploy, code, description, details, payload } = body;
 
@@ -88,7 +95,6 @@ app.route('/')
 
       res.status(200).send(data.jobId);
       console.log(JSON.stringify(data));
-
     } catch (error) {
       console.error(error);
       res.status(206).send(error);
@@ -97,23 +103,25 @@ app.route('/')
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}!`);
-})
+});
 
 exports.app = functions.https.onRequest(app);
 
 document = {
-  name: "",
-  page: "",
-  type: "",
-  version: "",
-  parameters: [{
-    key: "",
-    value: ""
-  }]
-}
+  name: '',
+  page: '',
+  type: '',
+  version: '',
+  parameters: [
+    {
+      key: '',
+      value: '',
+    },
+  ],
+};
 dataLayer = {
-  schema: "",
-  status: "",
-  objectName: "",
-  keyName: ""
-}
+  schema: '',
+  status: '',
+  objectName: '',
+  keyName: '',
+};
